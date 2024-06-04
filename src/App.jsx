373c1f8a04645b3caa100af734af591949fd007f
Mammadov <elmin.mammadov2@kapitalbank.az>
2024-06-04 17:37:50 +0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import FileUploader from './file-uploader'
+
+function App() {
+
+  return (
+    <FileUploader/>
+  )
+}
+
+export default App
---
+++ src/file-uploader.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import * as XLSX from 'xlsx';
+
+function FileUploader() {
+  const [data, setData] = useState([]);
+  const [validationResult, setValidationResult] = useState("");
+  const [selectedSchema, setSelectedSchema] = useState("typeA");
+
+  const schemas = {
+    typeA: {
+      ads: 'number',
+      ddd: 'number'
+    },
+    typeB: {
+      name: 'string',
+      surname: 'string'
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: 'buffer' });
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_json(ws);
+        validateData(data);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+  const validateData = (data) => {
+    const schema = schemas[selectedSchema];
+    let isValid = true;
+    for (let row of data) {
+      for (let key in schema) {
+        if (typeof row[key] !== schema[key]) {
+          isValid = false;
+          break;
+        }
+      }
+      if (!isValid) break;
+    }
+    if (isValid) {
+      setData(data);
+      setValidationResult("File is valid!");
+    } else {
+      setValidationResult(`Invalid file format for ${selectedSchema}.`);
+    }
+  };
+  console.log({
+    data
+  });
+  return (
+    <div>
+      <div>
+        <select onChange={(e) => setSelectedSchema(e.target.value)} value={selectedSchema}>
+          <option value="typeA">Type A</option>
+          <option value="typeB">Type B</option>
+        </select>
+      </div>
+      <input type="file" accept=".xlsx, .xls" onChange={handleFileChange} />
+      <div>{validationResult}</div>
+      {data.length > 0 && <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </div>
+  );
+}
+
+export default FileUploader;
